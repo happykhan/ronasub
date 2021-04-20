ef230942747af14fb1e9d63c42112e875e6d73ba
martinclott <martin.lott@quadram.ac.uk>
2021-04-20 11:08:06 +0100

    Second version of gather_plates.py, plate for some samples reported

--- metasub/gather_plates.py
+++ metasub/gather_plates.py
@@ -30,6 +30,7 @@ def gather():
 
     sampleName2Project=dict()
     sampleName2Called=dict()
+    sampleName2Plate=dict()
     sampleName2RunName=dict()
 
     for next_seq_directory in ('Nextseq_1_runs','Nextseq_2_runs'):
@@ -38,21 +39,42 @@ def gather():
             if os.path.isdir('transfer/incoming/QIB_Sequencing/' + next_seq_directory + '/' + directory):
                 sample_sheet_file = 'transfer/incoming/QIB_Sequencing/' + next_seq_directory + '/' + directory + '/SampleSheet.csv'
                 if os.path.isfile(sample_sheet_file):
-                    print(sample_sheet_file)
                     try:
+                        called2plate=dict()
+
                         with open(sample_sheet_file) as f:
                             lines = f.readlines()
+                            for line in lines:
+                                fields = line.rstrip().split(',')
+                                sampleName = fields[0]
+                                if (sampleName[-3:]=='_PC' or sampleName[-3:]=='_NC') and not sampleName[:4]=='ARCH':
+                                    called2plate[fields[2]] = sampleName[:sampleName.index('_')]
+                                elif sampleName[:5]=='BLANK': # Starts with blank
+                                    if '_' in sampleName: called2plate[fields[2]] = sampleName[sampleName.index('_')+1:]
+                                    else:
+                                        plate_name = sampleName[5:]
+                                        if plate_name[:1]=='c' or plate_name[:1]=='R': plate_name = plate_name[1:]
+                                        called2plate[fields[2]] = plate_name
+                                elif sampleName[-5:]=='BLANK': # Ends with blank
+                                    if '_' in sampleName: called2plate[fields[2]] = sampleName[:sampleName.index('_')]
+                                    else: called2plate[fields[2]] = sampleName[:-5]
+                            
                             for line in lines:
                                 fields = line.rstrip().split(',')
                                 sampleName = fields[0]
                                 if len(fields)>7:
                                     sampleName2Project[sampleName] = fields[len(fields)-1]
                                     sampleName2Called[sampleName] = fields[2]
+                                    
+                                    if fields[2] in called2plate.keys(): sampleName2Plate[sampleName] = called2plate[fields[2]]
+                                    elif sampleName[:8]=='ARCH-000' or sampleName[:8]=='ARCH_000': sampleName2Plate[sampleName] = sampleName[8:11]
+                                    else: sampleName2Plate[sampleName] = "Unknown"
+                                    
                                     sampleName2RunName[sampleName] = str(directory)
                     except:
                         pass
 
     for sampleName in sampleName2RunName.keys():
-        print(sampleName + ',' + sampleName2RunName[sampleName] + ',' + sampleName2Called[sampleName] + ',' + sampleName2Project[sampleName])
+        print(sampleName + ',' + sampleName2RunName[sampleName] + ',' + sampleName2Plate[sampleName] + ',' + sampleName2Called[sampleName] + ',' + sampleName2Project[sampleName])
 
 gather()
